f119607f1fe7f71d9177e913214a8118231d8b62
wasyl078 <wasilewski078@gmail.com>
2019-06-23 00:33:12 +0200

    new 1.1

--- NewGame/src/com/wasyl/NewGame/Framework/Frame.java
+++ NewGame/src/com/wasyl/NewGame/Framework/Frame.java
@@ -1,4 +1,6 @@
-public class Main {
+package com.wasyl.NewGame.Framework;
+
+public class Frame {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
---
+++ NewGame/src/com/wasyl/NewGame/Framework/Game.java
@@ -0,0 +1,4 @@
+package com.wasyl.NewGame.Framework;
+
+public class Game {
+}
